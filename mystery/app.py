5672a43069edaf3b552c2cc97252c4eea319087c
Andrew Ekstedt <ekstedta@oregonstate.edu>
2016-07-25 11:37:53 -0700

    Allow searching for different subjects
    No UI yet, but you can use the subject query parameter
    to pick which subject to choose from, or pick a random subject.

--- mystery/app.py
+++ mystery/app.py
@@ -10,8 +10,9 @@ from . import api
 
 TERM = '201603'
 SUBJECT = 'CS'
-SUBJECTNAME = 'Computer Science'
+CAMPUS = 'C' # Corvallis
 
+SUBJECTS = {}
 DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
 hour_re = re.compile(r'(?:[01][0-9]|2[0-3])(?:[0-5][0-9])')
@@ -21,21 +22,35 @@ app.config.from_object('mystery.defaults')
 if 'MYSTERY_CONFIG' in os.environ:
     app.config.from_envvar('MYSTERY_CONFIG')
 
+# TODO: use subjects API
+def load_subjects():
+    for line in app.open_resource('subjects'):
+        line = line.strip()
+        paren = line.index('(')
+        name = line[:paren].strip()
+        code = line[paren+1:-1].strip()
+        SUBJECTS[code] = name
+
+load_subjects()
+assert SUBJECT in SUBJECTS
+
 @app.route('/')
 def index():
-    try:
-        courses = get_all_courses(TERM, SUBJECT)
-        error = None
-    except api.APIError as e:
-        error = e.message
-        courses = []
-
-    courses = filter_courses(courses)
-
+    subject = SUBJECT
     now = datetime.now()
     day = DAYS[now.weekday()]
     hour = "{:02d}{:02d}".format(now.hour, now.minute)
-    all = ('all' in request.args)
+    all = ('all' in request.args) # for testing
+
+    if 'subject' in request.args:
+        if request.args['subject'] in SUBJECTS:
+            subject = request.args['subject']
+        elif request.args['subject'] == 'random':
+            # TODO don't choose a subject with no classes
+            subject = random.choice(SUBJECTS.keys())
+            return flask.redirect(flask.url_for('index', subject=subject))
+
+    subject_name = SUBJECTS[subject]
 
     if 'day' in request.args and request.args['day'] in DAYS:
         day = request.args['day']
@@ -43,11 +58,22 @@ def index():
     if 'hour' in request.args and hour_re.match(request.args['hour']):
         hour = request.args['hour']
 
+
+    try:
+        courses = get_all_courses(TERM, subject)
+        error = None
+    except api.APIError as e:
+        error = e.message
+        courses = []
+
+    courses = filter_courses(courses)
+
     if not all:
         courses = find_current_courses(courses, day, hour)
 
     if courses:
         # choose a random course
+        # TODO prefer large courses
         # TODO prefer courses that are not full
         random_course = random.choice(courses)
         meeting_time = get_meeting_time(random_course, day, hour)
@@ -61,7 +87,7 @@ def index():
         random_course=random_course,
         meeting_time=meeting_time,
         courses=courses,
-        subject_name=SUBJECTNAME,
+        subject_name=subject_name,
         number_of_courses=len(courses),
         error=error)
 
@@ -72,7 +98,7 @@ def get_all_courses(term, subject):
 
     page_number = 1
     while True:
-        result = client.search(TERM, SUBJECT, page_size=100, page_number=page_number)
+        result = client.search(term, subject, page_size=100, page_number=page_number)
         if u'data' not in result or u'links' not in result:
             raise ValueError('invalid results returned from class search')
         courses.extend(result[u'data'])
